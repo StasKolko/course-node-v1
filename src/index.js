5afc6bdbf0da3a773b07a555920423c2b2dfc086
StasDev <b2bstas@gmail.com>
2024-10-10 22:08:41 +0300

    add nodemon

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! 123');
 })
 
 app.listen(port, () => {
